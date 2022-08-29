3bfc4bae7706d84202abbb84e0d32d94bd77e686
NewBoss1 <58598186+NewBoss1@users.noreply.github.com>
2022-08-29 23:34:52 +0900

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const a = 5;
+const b = 2;
+let myName = "Blue_Yolk";
+
+console.log("Hello World!!!");
+console.log(a + b);
+console.log(a / b);
+console.log(a * b);
+console.log("Hello World!! My name is " + myName);
+
+myName = "GreenYolk";
+
+console.log("My name is " + myName);
+
+//const - 변하지 않는 변수(업데이트X)
+//let - 업데이트가 가능한 변수
+//var - 옛날 js에서 쓰던 변수. 규칙이 없기 때문에 의도가 파악되지 읺는다.
+//기본적으로 const를 쓰고 let은 필요할 때만 쓰되 ver은 쓰지 말 것.
